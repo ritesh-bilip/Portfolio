1948fdfea434a88e4529cdaa675319a3faf5197d
ritesh-bilip <dasritesh537@gmail.com>
2026-03-27 08:43:13 +0530

    Removed TODO.md, added build script and requirements, updated settings

--- protfolio/settings.py
+++ protfolio/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-xj-)dubx^b^_7(o4hmp&nu83*j(j!jnoe_#ejix_)+#r)wzh=7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
